865496c384704dede06b7591d4259bfa06683fea
Owen Smith <ogs22@cam.ac.uk>
2014-04-29 10:45:38 +0100

    filter

--- query.php
+++ query.php
@@ -2,7 +2,7 @@
 
 define("DB_HOST",'localhost');
 define("DB_USER",'cmep');
-define("DB_PASS",'88hwefce');
+define("DB_PASS",'@PASSWORD@');
 define("DB_NAME",'cmepsearch');
 header('Access-Control-Allow-Origin: *');
 
@@ -111,8 +111,6 @@ if ($_GET['search'] and $_GET['term']) {
 		print $search->outputJson();
 	}	
 }
-
-
 
 
 
